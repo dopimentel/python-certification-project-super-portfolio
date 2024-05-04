5374c76ff9e404d9ea835a819359694de8962a51
Marcos Pimentel <mpc_marcos@hotmail.com>
2024-05-04 01:26:03 -0300

    feat: Project Model

--- projects/models.py
+++ projects/models.py
@@ -10,3 +10,17 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Project(models.Model):
+    name = models.CharField(max_length=50, blank=False)
+    description = models.TextField(blank=False, max_length=500)
+    github_url = models.URLField(validators=[URLValidator()], blank=False)
+    keyword = models.CharField(max_length=50, blank=False)
+    key_skill = models.CharField(max_length=50, blank=False)
+    profile = models.ForeignKey(
+        Profile, on_delete=models.CASCADE, related_name="projects"
+    )
+
+    def __str__(self):
+        return self.name
